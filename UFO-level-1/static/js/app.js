57b46ada5fff14d7a7d00c56483807b4fe466c4e
Sydni <sydnimpierce@gmail.com>
2021-08-09 14:14:22 -0400

    added ;

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,21 +2,29 @@
 var tableData = data;
 
 // from index.html - link to objects
-var button = d3.select("#datetime")
+var button = d3.select("#datetime");
 
-var form = d3.select(".form-control")
+var form = d3.select(".form-control");
 
-var table = d3.select("tbody")
+var table = d3.select("tbody");
+
+var field = d3.select("input");
+
+var input = field.property("value");
 
 // event  handlers
-button.on("click", TableFilter)
-form.on("submit", TableFilter)
+button.on("click", TableFilter);
+form.on("submit", TableFilter);
 
 // create function to filter the table results
 function TableFilter() {
     d3.event.preventDefault();
 
-    table.html("")
+    table.html("");
+
+    tableData.filter() {
+
+    };
 
     tableData.forEach(function(sighting) {
         var row = table.append("tr");
