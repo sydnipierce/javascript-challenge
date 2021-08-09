e0910e15b6bdfb0a29711f67d4a649634ba03f29
Sydni <sydnimpierce@gmail.com>
2021-08-09 13:05:31 -0400

    created event handlers

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,8 +1,12 @@
 // from data.js
 var tableData = data;
 
+// from index.html - link to objects
 var button = d3.select("#datetime")
 
 var form = d3.select(".form-control")
 
-// YOUR CODE HERE!
+// event  handlers
+button.on("click", TableFilter)
+form.on("submit", TableFilter)
+
