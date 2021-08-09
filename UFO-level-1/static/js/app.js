869454392b791345785d65bb4eb902b162eab417
Sydni <sydnimpierce@gmail.com>
2021-08-09 14:33:45 -0400

    changed input name from value

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -2,27 +2,23 @@
 var tableData = data;
 
 // from index.html - link to objects
-var button = d3.select("#datetime");
+var button = d3.select("#filter-btn");
 
-var form = d3.select(".form-control");
-
-var table = d3.select("tbody");
-
-var field = d3.select("input");
-
-var input = field.property("value");
-
-// event  handlers
-button.on("click", TableFilter);
-form.on("submit", TableFilter);
+var form = d3.select("form");
 
 // create function to filter the table results
 function TableFilter() {
     d3.event.preventDefault();
 
-    table.html("");
+    var table = d3.select("tbody");
 
-    var tableFilter = tableData.filter(sighting => sighting.datetime === input);
+    var field = d3.select("input");
+
+    var fieldValue = field.property("value");
+
+    //table.html("");
+
+    var tableFilter = tableData.filter(sighting => sighting.datetime == fieldValue);
 
     tableFilter.forEach(function(sighting) {
         var row = table.append("tr");
@@ -32,3 +28,7 @@ function TableFilter() {
         });
       });
 };
+
+// event  handlers
+button.on("click", TableFilter);
+form.on("submit", TableFilter);
